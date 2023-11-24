5031c255ca5680e71af1c366500fbb7cd101ad17
Steven Wu <76638078+WXL-steven@users.noreply.github.com>
2023-11-25 02:31:42 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -94,6 +94,8 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
   "gpt-3.5-turbo-1106": "2023-04",
   "gpt-4-1106-preview": "2023-04",
+  "1-gpt-3.5-turbo-1106": "2023-04",
+  "2-gpt-4-1106-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
 };
 
